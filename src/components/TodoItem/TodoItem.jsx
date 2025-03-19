396b50279b283bb66cbde993b6a77fab21a0f567
pav-nk <pav_nk@vk.com>
2025-03-19 17:21:26 +0300

    Updating the application interface

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,28 +1,52 @@
-import { Card, IconButton, Flex } from "@chakra-ui/react";
+import { Component } from "react";
+import { CheckboxCard, IconButton, Flex, Text } from "@chakra-ui/react";
 import { LuStar, LuDelete } from "react-icons/lu";
 import "./TodoItem.css";
 
-const TodoItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? "tomato" : "white",
-    fontWeight: important ? "bold" : "normal",
-  };
+class TodoItem extends Component {
+  state = {};
 
-  return (
-    <Card.Root>
-      <Flex gap="2" p="2" alignItems="center">
-        <Card.Title flex="1" pl="2">
-          {label}
-        </Card.Title>
-        <IconButton aria-label="Important ToDo" variant="outline">
-          <LuStar />
-        </IconButton>
-        <IconButton aria-label="Delete ToDo" variant="outline">
-          <LuDelete />
-        </IconButton>
-      </Flex>
-    </Card.Root>
-  );
-};
+  render() {
+    const { label, important = false } = this.props;
+
+    const style = {
+      color: important ? "tomato" : "white",
+      fontWeight: important ? "bold" : "normal",
+    };
+
+    return (
+      <CheckboxCard.Root
+        size="md"
+        onChange={(e) => console.log(e.target.checked)}
+      >
+        <Flex alignItems="center" pr="2">
+          <CheckboxCard.Control flex="0">
+            <CheckboxCard.Indicator />
+          </CheckboxCard.Control>
+          <CheckboxCard.Label textStyle="lg" flex="1">
+            <Text textStyle="lg">{label}</Text>
+          </CheckboxCard.Label>
+          <Flex gap="2">
+            <IconButton
+              aria-label="Important ToDo"
+              variant="outline"
+              colorPalette="yellow"
+            >
+              <LuStar />
+            </IconButton>
+            <IconButton
+              aria-label="Delete ToDo"
+              variant="outline"
+              colorPalette="red"
+            >
+              <LuDelete />
+            </IconButton>
+          </Flex>
+        </Flex>
+        <CheckboxCard.HiddenInput />
+      </CheckboxCard.Root>
+    );
+  }
+}
 
 export default TodoItem;
